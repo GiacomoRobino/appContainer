adaaca01bb0bd873ed9a03d35394d9a0d5243feb
GiacomoRobino <30469325+GiacomoRobino@users.noreply.github.com>
2022-01-02 14:03:30 +0100

    feature: aggiunto canvas a hourglass

--- src/app/pages/apps/hourglass/hourglass.component.ts
+++ src/app/pages/apps/hourglass/hourglass.component.ts
@@ -10,6 +10,10 @@ export class HourglassComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let canvas = <HTMLCanvasElement>document.querySelector('canvas');
+    canvas.width = window.innerWidth * 0.87;
+    canvas.height = window.innerHeight * 0.70;
+    console.log(canvas.width, canvas.height);
   }
 
 }
