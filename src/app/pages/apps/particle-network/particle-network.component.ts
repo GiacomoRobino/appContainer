c023c13167f722c2ebc711b6b0eccad67e639882
GiacomoRobino <30469325+GiacomoRobino@users.noreply.github.com>
2022-01-08 16:00:10 +0100

    feature: added switch between two different type of net drawing in network particle

--- src/app/pages/apps/particle-network/particle-network.component.ts
+++ src/app/pages/apps/particle-network/particle-network.component.ts
@@ -8,6 +8,9 @@ import { Options } from '@angular-slider/ngx-slider';
   styleUrls: ['./particle-network.component.scss'],
 })
 export class ParticleNetworkComponent implements OnInit {
+  public netActivated = true;
+  public netType = "poly";
+  public renderedFrames = 0;
   public h: number = 0;
   public w: number = 0;
   public loopId: number = 0;
@@ -19,8 +22,8 @@ export class ParticleNetworkComponent implements OnInit {
     particleColor: 'rgba(255,255,255)',
     lineColor: 'rgba(0,181,255)',
     particleAmount: 4,
-    defaultRadius: 2,
-    variantRadius: 2,
+    defaultRadius: 0.5,
+    variantRadius: 0.5,
     defaultSpeed: 1,
     variantSpeed: 1,
     linkRadius: 100,
@@ -44,7 +47,9 @@ export class ParticleNetworkComponent implements OnInit {
   }
 
   reset() {
-	  this.particles.push(new Particle(this.w, this.h, this.options, this.ctx));
+    this.initializeElements();
+    this.startAnimation();
+	  //this.particles.push(new Particle(this.w, this.h, this.options, this.ctx));
   }
 
   initializeElements() {
@@ -57,7 +62,15 @@ export class ParticleNetworkComponent implements OnInit {
     window.requestAnimationFrame(this.animationLoop.bind(this));
   }
 
+  activateNet() {
+    this.netActivated = !this.netActivated;
+  }
+
   animationLoop() {
+    this.renderedFrames ++;
+    if (this.renderedFrames % 100 === 0) {
+      console.log(this.renderedFrames / 100);
+    }
     this.ctx.clearRect(0, 0, this.w, this.h);
     this.drawScene();
     requestAnimationFrame(this.animationLoop.bind(this));
@@ -78,26 +91,67 @@ export class ParticleNetworkComponent implements OnInit {
 
   drawLine() {
     for (let particle of this.particles) {
-      this.linkPoints(particle, this.particles);
+      if(this.netType === "poly") {
+      this.linkPointsPoly(particle, this.particles);}
+      else {
+        this.linkPoints(particle, this.particles);
+      }
     }
   }
 
+  
   linkPoints(point: any, hubs: any[]) {
+    point.radius = 0.5;
+    let particleNeighborsNumber = point.radius;
     for (var i = 0; i < hubs.length; i++) {
       var distance = this.checkDistance(point.x, point.y, hubs[i].x, hubs[i].y);
       var opacity = 1 - distance / this.options.linkRadius;
       if (opacity > 0) {
+        particleNeighborsNumber += 0.3;
+        if (this.netActivated){
         this.ctx.lineWidth = 0.5;
         this.ctx.strokeStyle = 'rgba(100,100,200,' + opacity + ')';
         this.ctx.beginPath();
         this.ctx.moveTo(point.x, point.y);
         this.ctx.lineTo(hubs[i].x, hubs[i].y);
         this.ctx.closePath();
-        this.ctx.stroke();
+        this.ctx.stroke();}
       }
     }
+    point.radius = particleNeighborsNumber;
   }
+  
 
+  linkPointsPoly(point: any, hubs: any[]) {
+    point.radius = 0.5;
+    let particleNeighborsNumber = point.radius;
+    let neighbors = [];
+    for (var i = 0; i < hubs.length; i++) {
+      var distance = this.checkDistance(point.x, point.y, hubs[i].x, hubs[i].y);
+      var opacity = 1 - distance / this.options.linkRadius;
+      if (opacity > 0) {
+        particleNeighborsNumber += 0.3;
+        if (this.netActivated){
+        this.ctx.lineWidth = 0.5;
+        this.ctx.strokeStyle = 'rgb(100,100,200)';
+        neighbors.push(hubs[i]);
+        }
+      }
+    }
+    if (neighbors.length > 0 && this.netActivated) {
+      this.ctx.beginPath();
+      for(let neighbor of neighbors) {
+        this.ctx.moveTo(point.x, point.y);
+        this.ctx.lineTo(neighbor.x, neighbor.y);
+        this.ctx.moveTo(point.x, point.y);}
+      this.ctx.closePath();
+      this.ctx.stroke();
+    }
+    point.radius = particleNeighborsNumber;
+  }
+  switchNetType() {
+    this.netType === "poly" ? this.netType = "single" : this.netType = "poly";
+  }
   checkDistance(x1: number, y1: number, x2: number, y2: number) {
     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
   }
