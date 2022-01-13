85f6c25555fd3c6dbc5be6be872a25585e586381
g.robino <g.robino@reply.it>
2022-01-13 09:43:21 +0100

    feature: started json to graph implementation

--- src/app/pages/apps/json-to-graph/json-to-graph.component.ts
+++ src/app/pages/apps/json-to-graph/json-to-graph.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ViewContainerRef, OnInit } from '@angular/core';
+import { forceSimulation} from 'd3-force'
+import { select, selectAll } from 'd3'
 @Component({
   selector: 'app-json-to-graph',
   templateUrl: './json-to-graph.component.html',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JsonToGraphComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private viewContainerRef:ViewContainerRef) { }
+  public svg : any;
+  public elem : any;
   ngOnInit(): void {
+    this.elem = this.viewContainerRef.element.nativeElement;
+      this.svg = select(this.elem).select("svg");
+      this.svg.append("rect").attr("height", 30).attr("width", 30).style("fill", "red");
   }
 
 }
