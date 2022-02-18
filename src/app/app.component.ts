ec149f6464a7358c04523e9032395b71bc462345
g.robino <g.robino@reply.it>
2022-02-18 10:54:42 +0100

    feature:added fix header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
-import { Component, ViewChild, OnInit, HostListener } from '@angular/core';
+import { Component, ViewChild, OnInit, HostListener, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('header') header: any;
+  @ViewChild('headerComponent') headerComponent: any;
+  @ViewChild('router') router: any;
   @HostListener('window:scroll', ['$event']) onScrollEvent() {
-
     let header = this.header.nativeElement;    
     let sticky = header.offsetTop;
     if (window.pageYOffset > sticky) {
       header.classList.add("sticky");
+      this.routerWidth = this.router.nativeElement.offsetWidth;
+      let width = this.routerWidth.toString() + "px";
+      console.log(width)
+      header.style.width = width;
     } else {
       header.classList.remove("sticky");
     }
   }
+
+  routerWidth = 0;
   title = 'app-container';
   backgroundCanvas: any;
 
@@ -28,7 +35,12 @@ export class AppComponent implements OnInit {
     ctx.fillRect(0, 0, this.backgroundCanvas.width, this.backgroundCanvas.height);
   }
 
+  ngAfterViewInit(){
+    
+    
+  }
+
   onActivate(event: any) {
-    this.header.setHome(Object.keys(event.router.rawUrlTree.queryParams).length !== 0);
+    this.headerComponent.setHome(Object.keys(event.router.rawUrlTree.queryParams).length !== 0);
   }
 }
