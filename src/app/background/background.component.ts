d98254cee15e196145a842edcb727aa099b0fdfa
GiacomoRobino <30469325+GiacomoRobino@users.noreply.github.com>
2022-02-21 21:19:52 +0100

    fix: implemented horizontal image switch

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -19,7 +19,7 @@ export class BackgroundComponent implements OnInit , AfterViewInit{
     this.svg = select('.background');
     this.svg.attr('shaper-rendering', 'crispEdges');
     Observable.fromEvent(document.body, 'mousemove').subscribe((e : any )=> {
-    let barHeight = 20;
+    let barHeight = 1;
     let x0 = e.clientX;
     let y0 = e.clientY;
     this.svg.selectAll('rect').remove();
@@ -28,24 +28,27 @@ export class BackgroundComponent implements OnInit , AfterViewInit{
     .attr('y', y0 - barHeight/2)
     .attr('width', 2000)
     .attr('height', barHeight)
-    .attr('fill', 'yellow');
+    .attr('fill', 'black');
    
     this.svg.append('rect')
     .attr('x', x0 - barHeight/2)
     .attr('y', 0)
     .attr('width',  barHeight)
     .attr('height', 2000)
-    .attr('fill', 'yellow');
+    .attr('fill', 'black');
     this.setImage(x0, y0);
     });
   }
 
   ngAfterViewInit() {
-    
-
   }
 
   setImage(widthNum :number, heightNum : number) {
+    let summer : any = select('.summer');
+    let x = summer._groups[0];
+    let image = x[0];
+    image.style.height = heightNum.toString() + "px";
+    //image.style.width = "55 vw";
   }
 
 }
