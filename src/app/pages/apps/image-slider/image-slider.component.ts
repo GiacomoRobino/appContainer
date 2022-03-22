2e5a0d06aa3d39a428800dd1596d8e33698f2a08
GiacomoRobino <30469325+GiacomoRobino@users.noreply.github.com>
2022-03-22 06:50:10 +0100

    Implement mouse position on background detection
    A listener that checks for the mouse position on the svg used as background for the image in image-slider application. It offsets the position of the image on the screen to give the correct coordinates

--- src/app/pages/apps/image-slider/image-slider.component.ts
+++ src/app/pages/apps/image-slider/image-slider.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { select, pointer } from 'd3';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-image-slider',
@@ -6,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-slider.component.scss']
 })
 export class ImageSliderComponent implements OnInit {
+  @ViewChild("background") bg! : any;
+  public svg : any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initSvg();
   }
+
+  initSvg() {
+    this.svg = select('.background');
+    this.svg.attr('shaper-rendering', 'crispEdges');
+
+    Observable.fromEvent(document.body, 'mousemove').subscribe((e : any )=> {
+    let barHeight = 20;
+
+    let coordinates = this.getMousePos(document.getElementById("background"), e);
+    let x0 = coordinates.x;
+    let y0 = coordinates.y;
+    console.log(x0, y0);
+    
+    this.svg.selectAll('rect').remove();
+    this.svg.append('rect')
+    .attr('x', 0)
+    .attr('y', y0 - barHeight/2)
+    .attr('width', x0)
+    .attr('height', barHeight)
+    .attr('fill', 'black');
+   
+    this.svg.append('rect')
+    .attr('x', x0 - barHeight/2)
+    .attr('y', 0)
+    .attr('width',  barHeight)
+    .attr('height', 2000)
+    .attr('fill', 'black');
+    });
+  }
+
+  getMousePos(element: any, event: any) {
+    
+    let x0 = event.clientX;
+    let y0 = event.clientY;
+    var rect = element.getBoundingClientRect();
+    return {
+      x: x0 - rect.left,
+      y: y0 - rect.top
+    };
+}
 
 }
